50c6e224fac4e608963d1c67db509d1f7ebbc401
Yuvaraj S <yuvayuvaraj720444@gmail.com>
2025-01-02 23:05:10 +0530

    folder structure

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import Header from './stable-components/Header.js'
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <h1 className='bg-gray-800 text-slate-200 text-3xl font-bold underline'>React App</h1>
+      <Header/> 
     </div>
   );
 }
--- src/stable-components/Header.js
+++ src/stable-components/Header.js
@@ -2,15 +2,16 @@
 
 export default function Header(){
     return <div className="p-5 bg-gray-800">
-        <div className="flex justify-between">
+        <div className="flex justify-between text-center">
             <div className="text-white text-2xl">YUVARAJ</div>
             <nav className="text-white">
-                <ul className="flex justify-start gap-2 text-slate-300">
+                <ul className="flex justify-start gap-2 text-slate-300 cursor-pointer">
                     <li className="pr-2 hover:text-white"><span className="text-purple-500">#</span>Home</li>
                     <li className="pr-2 hover:text-white"><span className="text-purple-500">#</span>Portfolio</li>
                     <li className="pr-2 hover:text-white"><span className="text-purple-500">#</span>Handcode Blogs</li>
                     <li className="pr-2 hover:text-white"><span className="text-purple-500">#</span>E-books</li>
                     <li className="pr-2 hover:text-white"><span className="text-purple-500">#</span>Connect Me</li>
+                    <li className="pr-2 hover:text-white"><span className="text-purple-500">#</span>Login</li>
                 </ul>
             </nav>
         </div>
