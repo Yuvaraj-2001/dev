971d209d175b38242bc7f5fa55564e538aae2108
Yuvaraj S <yuvayuvaraj720444@gmail.com>
2025-01-11 23:39:31 +0530

    added Mobile heading

--- src/pages/ui-reusables/Heading.js
+++ src/pages/ui-reusables/Heading.js
@@ -4,12 +4,12 @@ export default function Heading(prop){
 
     return <div className="py-2r flex justify-between items-center">
         <div className="flex items-center gap-6 ">
-            <h2 className="text-4r"><span className="text-purple-500">{prop.icon}</span> {prop.text}</h2>
-            {prop.line && <div className="h-1 w-96 bg-slate-200 rounded-sm"></div>}
+            <h2 className="text-4r mob:text-3r"><span className="text-purple-500">{prop.icon}</span> {prop.text}</h2>
+            {prop.line && <div className="h-1 w-96 bg-slate-200 rounded-sm mob:hidden"></div>}
         </div>
        
         {prop.arrow && 
-        <div className="flex gap-3 items-center cursor-pointer">
+        <div className="flex gap-3 items-center cursor-pointer mob:hidden">
             <span className="text-2r pl-1r">{prop.arrow}</span>
             <span>
                 <BsArrowRight size={30}/>
