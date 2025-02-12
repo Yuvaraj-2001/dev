be46020b8a9d81a91781f1f7961bb113256f2040
Yuvaraj S <yuvayuvaraj720444@gmail.com>
2025-02-13 02:19:17 +0530

    stackblitz

--- src/pages/stackblitz.js
+++ src/pages/stackblitz.js
@@ -12,6 +12,7 @@ function Stackblitz(){
         <div>
    
             <Heading icon={<FaJs color='#facc14' size={40}/>} text="" line={true} arrow="All collections"/>
+            <div className="border-yellow-400"></div>
             <div className="grid grid-cols-4 gap-6 py-2r">
                 {stackblitz.js.map((item, index) => (
                     <StackblitzCard key={index} icon={<FaJs color='#facc14' size={30}/>} color="yellow-400" props={item} />
@@ -29,8 +30,9 @@ function Stackblitz(){
 
         <div>
             <Heading icon={<RxJSIcon color='#F7DF1E'/>} text="" line={true} arrow="All collections"/>
+            <div className="border-pink-400"></div>
             <div className="grid grid-cols-4 gap-6 py-2r">
-                {stackblitz.react.map((item, index) => (
+                {stackblitz.rxjs.map((item, index) => (
                     <StackblitzCard key={index} icon={<RxJSIcon color='#F7DF1E' size={30}/>} color="pink-400" props={item} />
                 ))}
             </div>    
@@ -38,6 +40,7 @@ function Stackblitz(){
 
         <div>
             <Heading icon={<FaReact color='#60a5fa' size={40}/>} text="" line={true} arrow="All collections"/>
+            <div className="border-blue-400"></div>
             <div className="grid grid-cols-4 gap-6 py-2r">
                 {stackblitz.react.map((item, index) => (
                     <StackblitzCard key={index} icon={<FaReact color='#60a5fa' size={30}/>} color="blue-400" props={item} />
@@ -47,8 +50,10 @@ function Stackblitz(){
 
         <div>
             <Heading icon={<FaVuejs color='#47de7f' size={40}/>} text="" line={true} arrow="All collections"/>
+            <div className="border-green-400"></div>
             <div className="grid grid-cols-4 gap-6 py-2r">
-                {stackblitz.react.map((item, index) => (
+                {stackblitz.vue.map((item, index) => (
+                    
                     <StackblitzCard key={index} icon={<FaVuejs color='#47de7f' size={30}/>} color="green-400" props={item} />
                 ))}
             </div>    
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   presets: [],
+  // safelist: [{ pattern: /.*/ }],
   darkMode: 'media', // or 'class'
   theme: {
     accentColor: ({ theme }) => ({
